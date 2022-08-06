b3b3d00827be77af4fce067b1360a4ef1ae1bb58
SergioCaueGomes <caue.sergio@gmail.com>
2022-08-06 20:22:14 -0300

    First Program Java DIO

---
+++ IntrdEcossistemaDocJava/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgram {
+	
+	public static void main(String[] args) {
+		
+		Order order = new Order("code1234");
+		System.out.println(order);
+	}
+}
