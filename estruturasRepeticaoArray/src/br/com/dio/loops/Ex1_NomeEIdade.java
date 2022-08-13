56fcf2e20001057956d5606e9d3d0fa43cd7738a
SergioCaueGomes <caue.sergio@gmail.com>
2022-08-13 00:36:24 -0300

    Commit Ex1_NomeEIdade-comentado

--- estruturasRepeticaoArray/src/br/com/dio/loops/Ex1_NomeEIdade.java
+++ estruturasRepeticaoArray/src/br/com/dio/loops/Ex1_NomeEIdade.java
@@ -2,22 +2,34 @@ package br.com.dio.loops;
 
 import java.util.Scanner;
 
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare inserindo o valor 0 no campo nome)
+*/
+
 public class Ex1_NomeEIdade {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String nome;
-        int idade;
+        //Variaveis
+        String nome; //Armazena nome
+        int idade;  //Armazena idade
 
-        while (true) {
-        System.out.println("Nome: ");
-        nome = sc.next();
-        if (nome.equals("0")) break;
+        while (true) { //Enquando o loop for verdadeiro
+            System.out.println("Nome: "); //Peça o nome
+            nome = scan.next(); //armazene esse nome na variável
 
-        System.out.println("Idade: ");
-        idade = sc.nextInt();
+            if (nome.equals("0")) { //caso o nome igual a "0"
+                break; //interrompa o programa
+            }
+
+            System.out.println("Idade: "); //Peça a idade
+            idade = scan.nextInt(); //armazene a idade na variável
+
+            //imprima o nome e a idade
+            System.out.println("Nome: " + nome + " - idade: " + idade);
+
         }
-
-        System.out.println("Continua aqui após o while");
     }
 }
